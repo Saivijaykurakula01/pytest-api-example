80a821b33015446ddc5e5d2398f6f0d5e27ece85
Saivijaykurakula01 <Saivijaykurakula01@gmail.com>
2026-02-07 16:50:13 +0000

    Complete pytest API tests, OATCH order test and the HTML report

--- test_store.py
+++ test_store.py
@@ -1,16 +1,18 @@
-from jsonschema import validate
-import pytest
-import schemas
 import api_helpers
-from hamcrest import assert_that, contains_string, is_
+from hamcrest import assert_that, is_
 
-'''
-TODO: Finish this test by...
-1) Creating a function to test the PATCH request /store/order/{order_id}
-2) *Optional* Consider using @pytest.fixture to create unique test data for each run
-2) *Optional* Consider creating an 'Order' model in schemas.py and validating it in the test
-3) Validate the response codes and values
-4) Validate the response message "Order and pet status updated successfully"
-'''
 def test_patch_order_by_id():
-    pass
+    order_payload = {"pet_id": 0}
+    create_response = api_helpers.post_api_data("/store/order", order_payload)
+
+    assert create_response.status_code == 201
+    order_id = create_response.json()["id"]
+
+    patch_payload = {"status": "sold"}
+    patch_response = api_helpers.patch_api_data(f"/store/order/{order_id}", patch_payload)
+
+    assert patch_response.status_code == 200
+    assert_that(
+        patch_response.json()["message"],
+        is_("Order and pet status updated successfully")
+    )
